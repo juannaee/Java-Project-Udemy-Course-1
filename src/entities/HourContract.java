7ea04652db7ad65385a7f6ee6327aaeed5f3bf56
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2022-12-15 20:40:35 -0300

    HourContract Class Done

--- src/entities/HourContract.java
+++ src/entities/HourContract.java
@@ -1,5 +1,50 @@
 package entities;
 
+import java.util.Date;
+
 public class HourContract {
+
+	private Date contractDate;
+	private Double valuePerHour;
+	private Integer workedHours;
+
+	public HourContract() {
+
+	}
+
+	public HourContract(Date contractDate, Double valuePerHour, Integer workedHours) {
+		super();
+		this.contractDate = contractDate;
+		this.valuePerHour = valuePerHour;
+		this.workedHours = workedHours;
+	}
+
+	public Date getContractDate() {
+		return contractDate;
+	}
+
+	public void setContractDate(Date contractDate) {
+		this.contractDate = contractDate;
+	}
+
+	public Double getValuePerHour() {
+		return valuePerHour;
+	}
+
+	public void setValuePerHour(Double valuePerHour) {
+		this.valuePerHour = valuePerHour;
+	}
+
+	public Integer getWorkedHours() {
+		return workedHours;
+	}
+
+	public void setWorkedHours(Integer workedHours) {
+		this.workedHours = workedHours;
+	}
+
+	public Double TotalValue() {
+		return valuePerHour * workedHours;
+	}
 
 }
