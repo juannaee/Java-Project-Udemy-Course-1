1423b4df52f6d29a28f3b9a4570da90021a18a6a
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2022-12-15 20:29:55 -0300

    Finished Department class

--- src/entities/Department.java
+++ src/entities/Department.java
@@ -2,4 +2,23 @@ package entities;
 
 public class Department {
 
+	private String nameDepartment;
+
+	public Department() {
+
+	}
+
+	public Department(String nameDepartment) {
+		this.nameDepartment = nameDepartment;
+
+	}
+
+	public String getNameDepartment() {
+		return nameDepartment;
+	}
+
+	public void setNameDepartment(String nameDepartment) {
+		this.nameDepartment = nameDepartment;
+	}
+
 }
